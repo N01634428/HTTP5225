9a14232b75b7980b1f606d5cedb428e442dd1923
Vihar Thakkar <n01634428@humber.ca>
2025-05-14 16:36:35 -0400

    Week 2

---
+++ Week2/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+<!--Learning echo-->
+   <?php
+   
+     echo "<h1>Hello World!</h1>";
+
+     echo"<p> This is the first PHP Lab. </P>"
+
+
+   ?>
+   <hr>
+<!--Learning Varibles-->
+   <?php
+
+    $fname = "Good morning";
+    $lname = "Nice to meet you";
+
+    echo "<h1>Hello ". $fname . " " . $lname . ",</h1>"
+?>
+
+
+</body>
+</html>
