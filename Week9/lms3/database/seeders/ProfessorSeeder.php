5d81bed8c60cb986a876f334cf15d58bac6a2781
Vihar Thakkar <n01634428@humber.ca>
2025-08-13 14:57:11 -0400

    added professors in db

--- Week9/lms3/database/seeders/ProfessorSeeder.php
+++ Week9/lms3/database/seeders/ProfessorSeeder.php
@@ -2,17 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Professor;
 
 class ProfessorSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-   public function run(): void
-{
-    Professor::factory(10)->create();
-}
+    public function run(): void
+    {
+        Professor::factory()->count(10)->create();
+    }
 }
